3638ea49ac433b719a1ad56f9d06ab9665b6489c
udeaghad <dozie.udeagha@gmail.com>
2022-09-27 16:26:56 +0100

    Test posts#index http status

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      get posts_index_path
-      expect(response).to have_http_status(200)
+  describe "GET /posts#index" do
+  before(:example) { get user_posts_path(1) }
+    it "works! (checked http status)" do
+       expect(response).to have_http_status(200)
     end
   end
 end
