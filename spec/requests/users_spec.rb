aadcb417e9c58b9a60b4a052080c0eb6bd31bee8
udeaghad <dozie.udeagha@gmail.com>
2022-09-27 15:37:20 +0100

    test if the template render is successful

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (to test users index page)" do
-      get users_path
+    before(:example) { get users_path }
+    it "works! (checked https satus)" do
       expect(response).to have_http_status(200)
+    end
+
+    it "renders index " do
+      expect(response).to render_template('index')
     end
   end
 end
