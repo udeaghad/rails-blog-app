8f543fb480604ac237fb1ab2e60f487640f02336
udeaghad <dozie.udeagha@gmail.com>
2022-09-27 16:12:47 +0100

    to test users#show http status

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /users" do
+  describe "GET /users#index" do
     before(:example) { get users_path }
-    it "works! (checked https satus)" do
+    it "works! (checked https status)" do
       expect(response).to have_http_status(200)
     end
 
-    it "renders index " do
+    it "renders index template " do
       expect(response).to render_template('index')
     end
 
     it "page contains the correct text" do
       expect(response.body).to include('Here is a list of all users')
     end
+  end
+
+  describe "GET /users#show" do
+    before(:example) { get user_path(1) }
+    it "works! (checked https status)" do
+      expect(response).to have_http_status(200)
+    end
+
+   
   end
 end
