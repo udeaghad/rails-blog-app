8b167a452474d81ed0571febfdc9e7bcdd96449d
udeaghad <dozie.udeagha@gmail.com>
2022-09-27 15:07:09 +0100

    Perform http status test on users_path

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "User", type: :request do
+RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_index_path
+    it "works! (to test users index page)" do
+      get users_path
       expect(response).to have_http_status(200)
     end
   end
