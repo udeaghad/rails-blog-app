79972cd1fa3f06b18cc5261a2bf74aebf3a48087
udeaghad <dozie.udeagha@gmail.com>
2022-09-27 16:58:10 +0100

    To check if posts#show contains the coreect text

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -26,6 +26,8 @@ RSpec.describe "Posts", type: :request do
       expect(response).to render_template(:show)
     end
 
-   
+    it 'page contains the correct text' do
+      expect(response.body).to include('Here is a list of all posts for a given user and for a given post')
+    end   
   end
 end
