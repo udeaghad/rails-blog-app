2dee829fff25cd9fe74c9b7249f999144ee476ab
udeaghad <dozie.udeagha@gmail.com>
2022-09-29 21:04:26 +0100

    Add comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,11 @@
 class CommentsController < ApplicationController
 
+  def new
+    @comment = Comment.new
+    
+    respond_to do |format|
+      format.html { render :new, locals: { comment: @comment } }
+    end
+  end
+
 end
