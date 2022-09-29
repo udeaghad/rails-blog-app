269dfffccdcbfc90e6101f44aa6a042fbdac11e1
udeaghad <dozie.udeagha@gmail.com>
2022-09-29 21:52:52 +0100

    Create path for new comment and the link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "users#index"
       resources :users, only: [:index, :show] do
-        resources :posts, only: [:index, :show, :new, :create] do
-         resources :comments, only: [:new, :create]
-        end
+        resources :posts, only: [:index, :show, :new, :create]               
     end
+    get '/users/:user_id/posts/:id/new', to: 'comments#new', as: 'new_user_post_comment'
+    post '/users/:user_id/posts/:id', to: 'comments#create'
+
   end
