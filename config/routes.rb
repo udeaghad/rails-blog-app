060d149cb7e22f89de0a589b3d66d1a4c8fa5856
udeaghad <dozie.udeagha@gmail.com>
2022-09-30 13:14:26 +0100

    Create routes for likes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :show] do
         resources :posts, only: [:index, :show, :new, :create] do 
           resources :comments, only: [:new, :create]
+          resources :likes, only: [:new, :create]
         end              
-    end
-    # get '/users/:user_id/posts/:post_id/new', to: 'comments#new', as: 'new_user_post_comment'
-    # post '/users/:user_id/posts/:id', to: 'comments#create'
+    end    
 
   end
