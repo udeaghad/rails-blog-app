b1631c0781f4ad646f8f1778b0aa732b83bad71f
udeaghad <dozie.udeagha@gmail.com>
2022-09-30 12:43:22 +0100

    Create add comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,27 @@
 class CommentsController < ApplicationController
 
   def new
-    @comment = Comment.new
-    
+    @comment = Comment.new    
     respond_to do |format|
       format.html { render :new, locals: { comment: @comment } }
     end
+  end
+
+  def create
+    @comment = Comment.new(params.require(:comment).permit(:text).merge(users: current_user, posts_id: params.require(:post_id)))
+
+    respond_to do |format|
+      format.html do
+        if @comment.save
+          flash[:success] = "Comment created successfully"
+          redirect_to user_post_path(current_user, @comment.posts)
+        else
+          flash.now[:error] = "Error: Comment could not be saved"
+          redirect_to new_user_post_path(current_user)
+        end
+      end
+    end
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root "users#index"
       resources :users, only: [:index, :show] do
-        resources :posts, only: [:index, :show, :new, :create]               
+        resources :posts, only: [:index, :show, :new, :create] do 
+          resources :comments, only: [:new, :create]
+        end              
     end
-    get '/users/:user_id/posts/:id/new', to: 'comments#new', as: 'new_user_post_comment'
-    post '/users/:user_id/posts/:id', to: 'comments#create'
+    # get '/users/:user_id/posts/:post_id/new', to: 'comments#new', as: 'new_user_post_comment'
+    # post '/users/:user_id/posts/:id', to: 'comments#create'
 
   end
