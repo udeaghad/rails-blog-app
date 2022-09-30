0dc988fb89ecb1f3a8e0e24f056a60cf883117e4
udeaghad <dozie.udeagha@gmail.com>
2022-09-30 16:48:27 +0100

    Fix Linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,32 +6,31 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
-    
+
     respond_to do |format|
       format.html { render :new, locals: { post: @post } }
     end
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :text).merge(users: current_user, comments_counter: 0, likes_counter: 0))
+    @post = Post.new(params.require(:post).permit(:title, :text).merge(users: current_user, comments_counter: 0,
+                                                                       likes_counter: 0))
 
     respond_to do |format|
       format.html do
         if @post.save
-          flash[:success] = "Post created successfully"
+          flash[:success] = 'Post created successfully'
           redirect_to user_path(current_user)
         else
-          flash.now[:error] = "Error: Post could not be saved"
+          flash.now[:error] = 'Error: Post could not be saved'
           redirect_to new_user_post_path(current_user)
         end
       end
     end
-
   end
 
   def show
     @post = Post.where('id = ?', params[:id])
     @comments = Comment.where('posts_id = ?', params[:id])
-    
   end
 end
