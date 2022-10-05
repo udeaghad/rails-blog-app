7484320f23fc2be6ecb7f46d0d83fd6da462bc43
udeaghad <dozie.udeagha@gmail.com>
2022-10-05 09:16:40 +0100

    Update users#show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,5 @@ class ApplicationController < ActionController::Base
     def update_allowed_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :photo, :email, :password)}
       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :bio, :photo, :email, :password, :current_password)}
-    end
-  
-  
+    end  
 end
